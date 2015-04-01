a5358075c5af427b3d40a1507c938ae21a0444e0
Hydra <hectichydra@gmail.com>
2015-03-31 21:20:14 -0500

    Beta Launch

--- flatdarkness.meta.js
+++ flatdarkness.meta.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name          Flat Darkness Hack Forums Theme
 // @namespace     https://github.com/iHydra
-// @version       1.1.4
-// @description   Custom theme for Hack Forums.
+// @version       0.0.1
+// @description   Custom theme for Hack Forums. Base theme by Sasori.
 // @updateURL     https://raw.githubusercontent.com/iHydra/flatdarkness/master/flatdarkness.meta.js
 // @downloadURL   https://raw.githubusercontent.com/iHydra/flatdarkness/master/flatdarkness.user.js
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
 // @include       http://nsfw.hackforums.net/*
 // @include       http://www.nsfw.hackforums.net/*
-// @author        iHydra
-// @resource      Custom_CSS https://raw.githubusercontent.com/iHydra/flatdarkness/master/stylesheet_v.1.1.4.css
+// @author        iHydra/Sasori
+// @resource      CustomCSS https://raw.githubusercontent.com/iHydra/flatdarkness/master/stylesheet_v.0.0.1.css
 // @grant         GM_addStyle
 // @grant         GM_getResourceText
 // @run-at        document-start

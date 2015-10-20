ac27b714ba41849c259f1aa00eabcee8d134ff3c
Hydra <iHydra@users.noreply.github.com>
2015-10-20 10:05:25 -0500

    Update flatdarknessuser.js

--- flatdarknessuser.js
+++ flatdarknessuser.js
@@ -6,6 +6,8 @@
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
 // @author        iHydra/Kondax/Sasori
+// @updateURL     https://github.com/iHydra/flatdarkness/raw/master/flatdarkness.meta.js
+// @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdarkness.user.js
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
 // @resource      MainCSS https://raw.githubusercontent.com/iHydra/flatdarkness/master/stylesheet_1.5.0.css

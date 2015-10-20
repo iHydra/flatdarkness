a752c769e95a10e954ace5558e26d7792594c84c
Hydra <iHydra@users.noreply.github.com>
2015-10-20 10:34:09 -0500

    Update flatdarkness.meta.js

--- flatdarkness.meta.js
+++ flatdarkness.meta.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name          Flat Darkness - Development
+// @name          Flat Darkness - Stable
 // @namespace     https://github.com/iHydra
-// @version       DEV 1.5.0
+// @version       1.5.0
 // @description   Custom theme for Hack Forums. Base theme by Sasori.
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
 // @author        iHydra/Kondax/Sasori
-// @updateURL     https://github.com/iHydra/flatdarkness/raw/master/flatdev.meta.js
-// @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdev.user.js
+// @updateURL     https://github.com/iHydra/flatdarkness/raw/master/flatdarkness.meta.js
+// @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdarkness.user.js
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
-// @resource      MainCSS https://github.com/iHydra/flatdarkness/raw/master/stylesheet_dev.css?v=0519
+// @resource      MainCSS https://raw.githubusercontent.com/iHydra/flatdarkness/master/stylesheet_1.5.0.css
 // @resource      HLCSS https://github.com/isagalaev/highlight.js/raw/master/src/styles/monokai_sublime.css 
 // @grant         GM_addStyle
 // @grant         GM_getResourceText

2fba59bd6eb9d5e46e7e5b2f1f57f6fc7653903e
Hydra <iHydra@users.noreply.github.com>
2015-10-20 10:33:11 -0500

    Update flatdev.user.js

--- flatdev.user.js
+++ flatdev.user.js
@@ -6,6 +6,8 @@
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
 // @author        iHydra/Kondax/Sasori
+// @updateURL     https://github.com/iHydra/flatdarkness/raw/master/flatdev.meta.js
+// @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdev.user.js
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
 // @resource      MainCSS https://github.com/iHydra/flatdarkness/raw/master/stylesheet_dev.css?v=0519

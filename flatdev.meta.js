f19a733a00a1d557f4fbea3abb402e2bc5e090db
Hydra <iHydra@users.noreply.github.com>
2015-10-21 00:42:50 -0500

    Update flatdev.meta.js

--- flatdev.meta.js
+++ flatdev.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Flat Darkness - Development
 // @namespace     https://github.com/iHydra
-// @version       1.5.0d
+// @version       1.5.1a
 // @description   Custom theme for Hack Forums. Base theme by Sasori.
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
@@ -10,7 +10,7 @@
 // @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdev.user.js
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
-// @resource      MainCSS https://github.com/iHydra/flatdarkness/raw/master/stylesheet_dev.css?v=0519
+// @resource      MainCSS https://github.com/iHydra/flatdarkness/raw/master/stylesheet_dev.css?v=07316
 // @resource      HLCSS https://github.com/isagalaev/highlight.js/raw/master/src/styles/monokai_sublime.css 
 // @grant         GM_addStyle
 // @grant         GM_getResourceText

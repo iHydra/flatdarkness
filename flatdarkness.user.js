504a75ea02cc2aec3ba932ff1ad4a421928babc3
Hydra <iHydra@users.noreply.github.com>
2015-11-01 15:34:21 -0600

    Update flatdarkness.user.js

--- flatdarkness.user.js
+++ flatdarkness.user.js
@@ -11,7 +11,7 @@
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
 // @resource      MainCSS https://raw.githubusercontent.com/iHydra/flatdarkness/master/stylesheet_1.5.1.css
-// @resource      HLCSS https://github.com/isagalaev/highlight.js/raw/master/src/styles/monokai_sublime.css 
+// @resource      HLCSS https://raw.githubusercontent.com/isagalaev/highlight.js/master/src/styles/monokai-sublime.css 
 // @grant         GM_addStyle
 // @grant         GM_getResourceText
 // @run-at        document-start

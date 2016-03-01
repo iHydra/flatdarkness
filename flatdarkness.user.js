ffa777204fe6bbc79727ce4bc18cc33397468250
Hydra <iHydra@users.noreply.github.com>
2016-02-29 20:31:41 -0600

    Update flatdarkness.user.js

--- flatdarkness.user.js
+++ flatdarkness.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Flat Darkness - Stable
 // @namespace     https://github.com/iHydra
-// @version       1.5.4
+// @version       1.5.4a
 // @description   Custom theme for Hack Forums. Base theme by Sasori.
 // @include       http://www.hackforums.net/*
 // @include       http://hackforums.net/*
